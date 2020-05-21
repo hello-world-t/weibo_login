a64e4aef42a0b6dd85b0dc8dd63bcf8328201693
root <root@DESKTOP-7U44HDV.localdomain>
2020-05-21 17:30:28 +0800

    login weibo from win

---
+++ login_weibo_win.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+def main():
+    browser = webdriver.Chrome('C:/Users/mytjx/Desktop/chromedriver.exe')
+
+    browser.get("https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=https%3A%2F%2Fm.weibo.cn%2F")
+
+    try:
+        username_element = WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.ID,"loginName")))
+
+    except Exception as e:
+        print(e)
+        print("try again")
+
+
+    username_element = browser.find_element_by_id("loginName")
+    username_element.send_keys("15377071823")
+
+    userpwd_element = browser.find_element_by_id("loginPassword")
+    userpwd_element.send_keys("hyl110110")
+
+
+    login_element = browser.find_element_by_id("loginAction")
+    login_element.click()
+
+    time.sleep(30)
+
+
+
+if __name__ == "__main__":
+    main()
